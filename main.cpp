e82837e54188607576546c9d7376fc567e2ad288
Kim Ngo <kngo@nd.edu>
2014-06-10 14:03:17 -0400

    Asks for file input to calculate CF and MinMax

--- main.cpp
+++ main.cpp
@@ -15,15 +15,34 @@
 #include <sstream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
+
+//	vector< pair<string, pair<ExtractSequence, CodonFrequency> > >;
+	vector <char *> genomeFileNames;
+	vector<ExtractSequence> genomeSeqs;
+	string filename = "empty";
+
+	// Enter genome files
+	cout << "Enter genome file names to calculate codon frequency or press enter to continue: ";
+	while(!filename.empty()) {
+  	cin >> filename;
+	  if(!filename.empty()) {
+			char *file = &filename[0];
+			ExtractSequence Seqs(file);				// Extract sequences from file
+			genomeSeqs.push_back(Seqs);				// Adds sequences to vector of genomes
+			genomeFileNames.push_back(file);	// Adds genome file names to vector
+  	}
+	}
+	
+	vector<char *>::iterator genomes_it = genomeFileNames.begin();
+	for(int genomeSeqs_it = 0; genomes_it != genomeFileNames.end(); genomes_it++, genomeSeqs_it++) {
+	  CodonFrequency CF(*genomes_it, genomeSeqs[genomeSeqs_it].getVectorOfSequences());
+  	MinMax calcMinMax(*genomes_it, genomeSeqs[genomeSeqs_it].getVectorOfSequences(), CF);
+  }
   
-  char *filename = "Ypes.fasta";
-// testing to verify the frequency calculations
-  ExtractSequence Seqs(filename);
-  CodonFrequency CF(filename, Seqs.getVectorOfSequences());
-  MinMax calcMinMax(filename, Seqs.getVectorOfSequences(), CF);
 	return 0;
 }
